e6838ed33ea30db434f71acf3ec75d28f130048e
brettmadrid <brettmadrid9@gmail.com>
2020-02-10 21:48:59 -0800

    completed dft method

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -51,7 +51,16 @@ class Graph:
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        s = Stack()
+        s.push(starting_vertex)
+        visited = set()
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for next_v in self.get_neighbors(v)
+                s.push(next_v)
 
     def dft_recursive(self, starting_vertex):
         """
