6167d792f28c9aaf8e6c053004267959460564fd
brettmadrid <brettmadrid9@gmail.com>
2020-02-10 22:02:55 -0800

    completed dft_recursive method

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -62,14 +62,20 @@ class Graph:
                 for next_v in self.get_neighbors(v):
                     s.push(next_v)
 
-    def dft_recursive(self, starting_vertex):
+    def dft_recursive(self, starting_vertex, visited=None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+        visited.add(starting_vertex)
+        print(starting_vertex)
+        for neighbor in self.vertices[starting_vertex]:
+            if neighbor not in visited:
+                self.dft_recursive(neighbor, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
