ff2ba36cc5831ad4df8a0d38bb4491197c07e967
brettmadrid <brettmadrid9@gmail.com>
2020-02-12 00:30:34 -0800

    completed earliest ancestor all tests pass

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -33,4 +33,31 @@ class Graph:
 
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+    for pair in ancestors:
+        # add vertices
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+
+        # add edges, but in reverse order
+        graph.add_edge(pair[1], pair[0])
+
+    # Now do a BFS
+    q = Queue()
+    q.enqueue([starting_node])
+    path_length = 1
+    earliest_ancestor = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        # if (path is equal or longer and the value is smaller)
+        # or (if the path is longer)
+        if (len(path) >= path_length and v < earliest_ancestor) or (len(path) > path_length):
+            earliest_ancestor = v
+            path_length = len(path)
+        for neighbor in graph.vertices[v]:
+            new_path = list(path)
+            new_path.append(neighbor)
+            q.enqueue(new_path)
+
+    return earliest_ancestor
