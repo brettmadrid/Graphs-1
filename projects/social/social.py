90eed82aa264d99005b9c555f902334a3b3cc406
Brett Madrid <brettmadrid@Bretts-MacBook-Air.local>
2020-02-17 18:40:58 -0800

    completed populate_graph in social.py

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,30 @@
+import random
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, vaue):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if (self.size()) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
+    def __repr__(self):
+        return self.name
+
 
 class SocialGraph:
     def __init__(self):
@@ -14,11 +38,14 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
             print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -42,11 +69,30 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
+        # 100 users, avg 10 friendships each
+        # avg_friendships = total_friendships / num_users
+        
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i+1}")
 
-        # Create friendships
+        # create N random friendships, with all possible friendship combinations
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        # shuffle the list
+        random.shuffle(possible_friendships)
+        
+        print(possible_friendships)
+
+        # then grab the first N elements from the list.
+        # number of times to call add_friendships = avg_friendships * num_users / 2
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -65,6 +111,9 @@ class SocialGraph:
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
+    print("-----")
+    print(sg.users)
+    print("-----")
     print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+    # connections = sg.get_all_social_paths(1)
+    # print(connections)
